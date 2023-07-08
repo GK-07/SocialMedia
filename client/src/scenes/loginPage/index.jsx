198182f2f097aa969b6cce6827c5c016aa3f193a
Gaurav Galiyawala <galiyawalagaurav@gmail.com>
2023-07-08 19:54:01 +0530

    Done all the changes created login and registration

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,39 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import Form from "./Form";
+
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        boxShadow="0px 0px 10px rgba(0, 0, 0, 0.1)"
+        padding="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Facepedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreen ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        boxShadow="0px 0px 10px rgba(0, 0, 0, 0.1)"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Facepedia, The Social Network for Faces !
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
   );
 };
 
