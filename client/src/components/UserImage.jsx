e086a16792cecf25db6c55f6a5f670e989ec061b
Gaurav Galiyawala <galiyawalagaurav@gmail.com>
2023-07-11 19:08:38 +0530

    Alot of changes, added add post option

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -4,8 +4,9 @@ const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
       <img
-        sx={{ objectFit: "cover", borderRadius: "50%" }}
+        style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
+        height={size}
         alt="user"
         src={`http://localhost:3001/assets/${image}`}
       />
